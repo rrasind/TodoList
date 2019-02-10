117fbf25d2fefe10832377273457025a9e3aa237
Rasind Raveendran <rrasind123@gmail.com>
2019-02-10 17:49:26 +0530

    Initial commit

---
+++ myweb/todo/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,HttpResponse,redirect
+from .form import ListForm
+from .models import List
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ('Item has been added'))
+            return render(request, 'home.html',{'all_items': all_items})
+    else:
+        all_items = List.objects.all
+        return render(request, 'home.html', {'all_items': all_items})
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Deleted'))
+    return redirect('home')
+
+def cross_off(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('home')
+
+def uncross(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
